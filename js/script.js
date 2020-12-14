738c50bf958035f9f3bd14e59e4fb28d2c05eb85
Mahin <arrafi.mahin@gmail.com>
2020-12-14 18:47:03 +0600

    added subscription button js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,6 @@ var mail
 function getmail()
 {
   mail  = document.getElementById("get_mail").value;
+  document.getElementById('sub_btn').innerHTML="Submitted";
+  console.log('mail submitted successfully.');
 }
